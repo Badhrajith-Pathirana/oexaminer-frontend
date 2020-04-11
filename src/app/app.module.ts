c224170cfc16ae023bb11c62cc32baf583575850
Chinthani <chinthani96@gmail.com>
2020-04-11 14:13:37 +0530

    Deleted test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ExamdetailsComponent } from './examdetails/examdetails.component';
 import { AppRoutingModule } from './app-routing.module';
-import { TestComponent } from './test/test.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ExamdetailsComponent,
-    TestComponent
+    
   ],
   imports: [
     BrowserModule,

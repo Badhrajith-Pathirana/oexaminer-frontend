1a5c7cdb3ce5204cfd69c856d547d7159dda0974
Chinthani <chinthani96@gmail.com>
2020-04-11 12:57:07 +0530

    Routing module and component to create exams

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExamdetailsComponent } from './examdetails/examdetails.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamdetailsComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
